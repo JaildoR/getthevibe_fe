e3a65dfbe05e6133890ee14e1330e2b7ed9bed2e
Jaildo Rocha <j.vieirarocha@gmail.com>
2022-03-08 15:44:37 -0300

    fully conected with api

--- base_app.py
+++ base_app.py
@@ -1,8 +1,10 @@
+import base64
 import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
 from PIL import Image
+import json
 
 #config
 img = Image.open('streamlit-img/haut_de_page.jpg')
@@ -13,7 +15,7 @@ logo = Image.open('streamlit-img/get the vibe .png')
 st.image(logo)
 
 #file downloader
-file= st.file_uploader("Upload Image", type=["png","jpg","jpeg"])
+file= st.file_uploader("", type=["png","jpg","jpeg"])
 
 #shows the picture if there is one
 if file == None :
@@ -22,9 +24,10 @@ else:
     # file_details = {"filename":file.name, "filetype":file.type,
     #                          "filesize (in MB)":file.size / 1000000}
     # st.write(file_details)
+
+    file_bytes = file.getvalue()
     image = Image.open(file)
     st.image(image, width = 400)
-
 #uploading image from your camera
 st.header('or upload directly from your camera:')
 picture = st.camera_input("Take a picture")
@@ -41,29 +44,14 @@ st.markdown(hide_ad, unsafe_allow_html = True)
 
 if st.button('Check the vibe'):
     url = 'http://127.0.0.1:8000/vibecheck'
-    headers = {'Content-Type': 'multipart/form-data'}
-    file_post = {'file': file}
+    headers = {'Content-Type': 'application/json',
+               'Accept': 'text/plain'}
+
+    file_post = {'file': file_bytes}
     response = requests.post(url, headers,files = file_post)
-
-    st.write(response.status_code)
+    results = response.content.decode('utf-8')
+    results = json.loads(results)
+    st.header('The emotion is:')
+    st.subheader(results["emotion"])
 else:
-    st.write('Goodbye')
-
-#if we want to download the file in the computer
-#if choice == "Image":
-#
-#		st.subheader("Image")
-#			type=["png","jpg","jpeg"])
-#
-#		if file is not None:
-        # TO See details
-#			  file_details = {"filename":file.name, "filetype":file.type,
- #                             "filesize":file.size}
-#			  st.write(file_details)
-#			  st.image(load_image(image_file), width=250)
-#
-#			  #Saving upload
-#			  with open(os.path.join("fileDir",file.name),"wb") as f:
-#			  	f.write((file).getbuffer())
-#
-#			  st.success("File Saved")
+    pass
