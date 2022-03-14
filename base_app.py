8cd2c817813f40079e9a9157b0e73c02cfc46122
Pilar Figueroa <pifigueroa7@gmail.com>
2022-03-14 00:31:02 -0300

    Functioning CSS stylesheet + disappering GIF with result

--- base_app.py
+++ base_app.py
@@ -7,7 +7,7 @@ import json
 
 #config
 img = Image.open('streamlit-img/haut_de_page.jpg')
-st.set_page_config(page_title = 'Get The Vibe', page_icon = img)
+st.set_page_config(page_title = 'Get The Vibe', page_icon = img, layout="centered")
 #title of the app
 logo = Image.open('streamlit-img/get the vibe .png')
 st.image(logo)
@@ -18,7 +18,14 @@ hide_ad = """
         </style>
         """
 st.markdown(hide_ad, unsafe_allow_html = True)
+#
 
+with open('css/style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+#st.markdown('<style>body{color: hotpink;}</style>', unsafe_allow_html=True)
+
+# body
 col1, col2, col3 = st.columns(3)
 
 with col1:
@@ -28,7 +35,7 @@ with col2:
     page = st.select_slider("",options=page_names)
 
     #diff pages for image or camera photo
-   # page_names = ['File Uploader', 'Camera Photo']
+    # page_names = ['File Uploader', 'Camera Photo']
     #page = st.radio('Choose one', page_names)
 with col3:
     pass
@@ -52,38 +59,37 @@ if page == 'File Uploader':
         image = Image.open(file)
         st.image(image, width = 400)
         if st.button('😀 Get the vibe 😀'):
-            gif = st.markdown("![Alt Text](https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif)")
+            gif = st.image('https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif')
             url = 'https://vibe-opf4327g5q-ew.a.run.app/vibecheck'
             headers = {'Content-Type': 'application/json',
-               'Accept': 'text/plain'}
+            'Accept': 'text/plain'}
             file_post = {'file': file_bytes}
             response = requests.post(url, headers,files = file_post)
             results = response.content.decode('utf-8')
             results = json.loads(results)
 
-            if results :
-                del gif
-                st.header('The emotion is:')
-                st.subheader(results["emotion"])
-            else:
-                pass
+            st.header('I detect...')
+            st.subheader(results["emotion"])
+
 
 else :
     picture = st.camera_input("Take a picture")
     if picture:
-        st.image(picture)
+        #st.image(picture)
         if st.button('😀 Get the vibe 😀'):
-            st.markdown("![Alt Text](https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif)")
+            #gif = st.image('https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif')
+            gif = st.markdown('<img src="https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif" class="loading">', unsafe_allow_html=True)
+            #st.markdown("![Alt Text](https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif)")
             url = 'https://vibe-opf4327g5q-ew.a.run.app/vibecheck'
             headers = {'Content-Type': 'application/json',
-               'Accept': 'text/plain'}
+            'Accept': 'text/plain'}
             file_post = {'file': picture}
             response = requests.post(url, headers,files = file_post)
+            gif.empty()
             results = response.content.decode('utf-8')
             results = json.loads(results)
-            st.header('The emotion is:')
+            st.header('I detect...')
             st.subheader(results["emotion"])
-
 
 st.write('test')
 #changing button color
