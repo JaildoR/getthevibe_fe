11c3a5b8d9e454beb371f6cabf878b1a046ed73a
Jaildo Rocha <j.vieirarocha@gmail.com>
2022-03-15 13:08:44 -0300

    correct the quotes

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ primaryColor = '#0097A7'
 backgroundColor = '#D9534F'
 secondaryBackgroundColor = '#D9534F'
 textColor= '##00474f'
-font = ‘sans serif’
+font = 'sans serif'
 [server]
 headless = true
 port = $PORT
